5dadf89eb2eba52b7193244ca572a39d6855814b
lucas-mrq <lucasmarquet63800@gmail.com>
2022-08-02 23:30:35 +0200

    Adjust height size

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import "../styles.css";
 
 const ContentArea = styled.main`
-  height: calc(100% - 46px - 25px);
+  height: ${(props) => (props.showMenu === 0 ? "calc(100vh - 175.6px - 40px)" : "calc(100vh - 46px - 40px)")};
   ::-webkit-scrollbar {
     display: none; /* Chrome Safari */
   }
@@ -151,7 +151,7 @@ const Layout = ({ pageTitle, language, children }) => {
             </PhoneLayout>
           }
       </nav>
-      <ContentArea>
+      <ContentArea showMenu={showMenu ? 0 : 1}>
         {children}
       </ContentArea>
       <BottomArea>
