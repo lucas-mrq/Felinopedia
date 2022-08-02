89bd1201d726c25258a7a95f41229ef7bf5974d3
lucas-mrq <lucasmarquet63800@gmail.com>
2022-08-02 23:58:15 +0200

    Adjust number 6

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,6 +19,7 @@ const NavLink = styled.ul`
   list-style: none;
   padding: 0;
   width: 100%;
+  margin-top: 16px;
 `;
 const Button = styled.div`
   background-color: ${(props) => (props.current===0 ? `#FFCBA5;` : "#FFE6B5;")}
