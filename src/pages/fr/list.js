b940de5ac0c6bba7d0f15f62d9665a6c801c21db
lucas-mrq <lucasmarquet63800@gmail.com>
2022-08-04 12:27:49 +0200

    Add ocelots and some zoo to map

--- src/pages/fr/list.js
+++ src/pages/fr/list.js
@@ -19,7 +19,6 @@ const MapArea = styled.div`
 const Image = styled.div`
   position:relative;
   z-index: 1;
-  background-color: red;
 `
 const Zoo = styled.div`
   position: relative;
@@ -63,16 +62,22 @@ const Liste = () => {
                 src="../../images/map-france.png"
               />
             </Image>
-            <Zoo name="Zoo de Vincennes" color="#5A763A" border="#69675B" position={["-100","75"]} zIndex={30} onClick={() => setSelectedZoo("Vincenes")}>.</Zoo>
+            <Zoo name="Zoo Amiens" color="#3e8223" border="#000000" position={["-98","7"]} zIndex={31} onClick={() => setSelectedZoo("Amiens")}>.</Zoo>
+            <Zoo name="Zoo de Vincennes" color="#5A763A" border="#69675B" position={["-6","75"]} zIndex={30} onClick={() => setSelectedZoo("Vincenes")}>.</Zoo>
             <Zoo name="Zoo Amneville" color="#000000" border="#7BBEE5" position={["4","53"]} zIndex={27} onClick={() => setSelectedZoo("Amneville")}>.</Zoo>
-            <Zoo name="Parrot World" color="red" border="blue" borderForm="top" position={["4","15"]} zIndex={24} onClick={() => setSelectedZoo("Lumigny")}>.</Zoo>
+            <Zoo name="Parrot World" color="red" border="blue" borderForm="top" position={["4","15"]} zIndex={24} onClick={() => setSelectedZoo("Crécy")}>.</Zoo>
             <Zoo name="Parc des Felins" color="#F67D21" border="#99A63A" borderForm="bottom" position={["0","15"]} zIndex={21} onClick={() => setSelectedZoo("Lumigny")}>.</Zoo>
-            <Zoo name="Zoo de Beauval" color="#006BB1" border="#FED30E" position={["9","-7"]} zIndex={18} onClick={() => setSelectedZoo("Beauval")}>.</Zoo>
+            <Zoo name="Zoo de la Flèche" color="#FFFFFF" border="#ffbf00" position={["4","-33"]} zIndex={19} onClick={() => setSelectedZoo("Fleche")}>.</Zoo>
+            <Zoo name="Zoo de Beauval" color="#006BB1" border="#FED30E" position={["1","-7"]} zIndex={18} onClick={() => setSelectedZoo("Beauval")}>.</Zoo>
             <Zoo name="Zoo de la Palmyre" color="#386D1D" border="#000000" position={["12","-44"]} zIndex={15} onClick={() => setSelectedZoo("La Palmyre")}>.</Zoo>
-            <Zoo name="Zoo de Pessac" color="#FFFFFF" border="#ca0f47" position={["2","-38"]} zIndex={15} onClick={() => setSelectedZoo("Pessac")}>.</Zoo>
-            <Zoo name="Domaine des Fauves" color="#FFFFFF" border="#7BBEE5" position={["-7","54"]} zIndex={12} onClick={() => setSelectedZoo("Abrets-en-Dauphiné")}>.</Zoo>
+            <Zoo name="Zoo de Pessac" color="#FFFFFF" border="#ca0f47" position={["2","-38"]} zIndex={14} onClick={() => setSelectedZoo("Pessac")}>.</Zoo>
+            <Zoo name="Zoo Arcachon" color="#FFFFFF" border="#6d4931" position={["0","-45"]} zIndex={13} onClick={() => setSelectedZoo("Arcachon")}>.</Zoo>
+            <Zoo name="Domaine des Fauves" color="#FFFFFF" border="#7BBEE5" position={["-12","54"]} zIndex={12} onClick={() => setSelectedZoo("Abrets-en-Dauphiné")}>.</Zoo>
             <Zoo name="Zoo African Safari" color="#FFFFFF" border="#000000" position={["17","-11"]} zIndex={9} onClick={() => setSelectedZoo("African Safari")}>.</Zoo>
-            <Zoo name="Zoo du Carbet " color="#e11b22" border="#b9d433" position={["11","-60"]} zIndex={2} onClick={() => setSelectedZoo("Carbet")}>.</Zoo>
+            <Zoo name="Parc Animalier des Pyrénées" color="#FFFFFF" border="#83b91e" position={["-2","-28"]} zIndex={8} onClick={() => setSelectedZoo("Ayzac-Ost")}>.</Zoo>
+            <Zoo name="Parc Asson" color="#379225" border="#feb835" position={["-5","-35"]} zIndex={7} onClick={() => setSelectedZoo("Asson")}>.</Zoo>
+            <Zoo name="Zoo du Carbet" color="#e11b22" border="#b9d433" position={["8","-60"]} zIndex={4} onClick={() => setSelectedZoo("Carbet")}>.</Zoo>
+            <Zoo name="Zoo de Macouria " color="#fff0d3" border="#ef8f12" position={["-5","-30"]} zIndex={3} onClick={() => setSelectedZoo("Macouria")}>.</Zoo>
           </MapArea>
         </Flex>
     </Layout>
