73a586b370a8354db98dc60c0b84820a0cd8a535
lucas-mrq <lucasmarquet63800@gmail.com>
2023-03-21 13:52:30 +0000

    Create 2subfolders for article and animals

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import styled from "styled-components"
-import { StaticImage } from 'gatsby-plugin-image'
-
-const Image = styled.div`
-  width: 50%;
-  border-radius: 5px;
-  background-color: #FFCBA5;
-  padding: 5px;
-  margin-left: 10px;
-  margin-top: 10px;
-  max-width: 500px;
-`
-const Main = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
-`
 
 const IndexPage = ({ data }) => {
-  return (
-    <Layout pageTitle="Acceuil" language="french">
-      <Main>
-        <Image>
-          <StaticImage alt="french flag" src="../../data/bienvenue/welcome.jpg"/>
-        </Image>
-      </Main>
-    </Layout>
-  )
+  return (<Layout pageTitle="Acceuil" language="french"/>)
 }
 export default IndexPage
